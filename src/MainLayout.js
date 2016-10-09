6a4580d04651e45072f9e1e66ea5f9e8ceff5bc6
Vlad Golubev <golubev.vld@gmail.com>
2016-10-09 18:17:32 +0300

    Highlight active menu item

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -7,7 +7,8 @@ import {
   Navigation,
   Content
 } from 'react-mdl'
-import {Link} from 'react-router';
+import { Link } from 'react-router';
+import './MailLayout.css';
 
 class MainLayout extends Component {
   render() {
@@ -16,10 +17,14 @@ class MainLayout extends Component {
         <Header title="ТНЕУ"/>
         <Drawer title="">
           <Navigation>
-            <Link to="/news"><IconButton name="list"/>&nbsp;Новини</Link>
-            <Link to="/map"><IconButton name="map"/>&nbsp;Карта</Link>
-            <a href="http://modulok.ml/" target="_blank"><IconButton name="filter_5"/>&nbsp;Оцінки</a>
-            <Link to=""><IconButton name="settings_overscan"/>&nbsp;Сканер QR кодів</Link>
+            <Link to="news" activeClassName="active"><IconButton
+              name="list"/>&nbsp;Новини</Link>
+            <Link to="map" activeClassName="active"><IconButton
+              name="map"/>&nbsp;Карта</Link>
+            <a href="http://modulok.ml/" target="_blank"><IconButton
+              name="filter_5"/>&nbsp;Оцінки</a>
+            <Link to="" activeClassName="active"><IconButton
+              name="settings_overscan"/>&nbsp;Сканер QR кодів</Link>
           </Navigation>
         </Drawer>
         <Content>
